490f84b19a212e1cbb4dfd38998cecdaaf1a890b
Iceygirl <1427882109@qq.com>
2019-05-07 20:08:12 +0800

    更新代码

---
+++ src/pages/babel/js/index.js
@@ -0,0 +1,8 @@
+let a = 1
+console.log(a)
+
+const show = ()=>{
+    console.log('111')
+}
+
+show()
---
+++ src/pages/babel/lib/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var a = 1;
+console.log(a);
+
+var show = function show() {
+  console.log('111');
+};
+
+show();
